8432326a5db86cdbf54193f8a54ec9c78d3b0209
ahmed-bs <medamed777@gmail.com>
2021-07-26 12:47:45 +0000

    right side widget ..

--- reactproject/src/components/WidgetLg/WidgetLg.jsx
+++ reactproject/src/components/WidgetLg/WidgetLg.jsx
@@ -1,6 +1,9 @@
 import "./widgetLg.css"
 
 export default function WidgetLg() {
+    const Button = ({type})=>{
+        return <button className={"widgetLgButton " + type} >{type}</button>
+    }
     return (
         <div className="widgetLg">
             <h3 className="widgetLgTitle">Latest Transactions</h3>
@@ -10,6 +13,33 @@ export default function WidgetLg() {
                     <th className="widgetLgTh">Date</th>
                     <th className="widgetLgTh">Amount</th>
                     <th className="widgetLgTh">Status</th>
+                </tr>
+                <tr className="widgetLgTable">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Mary Carot</span>
+                    </td>
+                    <td className="widgetLgDate">2 juin 2021</td>
+                    <td className="widgetLgAmount">$47</td>
+                    <td className="widgetLgStatus"><Button type="Approved" /></td>
+                </tr>
+                <tr className="widgetLgTable">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Mary Carot</span>
+                    </td>
+                    <td className="widgetLgDate">2 juin 2021</td>
+                    <td className="widgetLgAmount">$47</td>
+                    <td className="widgetLgStatus"><Button type="Declined" /></td>
+                </tr>
+                <tr className="widgetLgTable">
+                    <td className="widgetLgUser">
+                        <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="widgetLgImg" />
+                        <span className="widgetLgName">Mary Carot</span>
+                    </td>
+                    <td className="widgetLgDate">2 juin 2021</td>
+                    <td className="widgetLgAmount">$47</td>
+                    <td className="widgetLgStatus"><Button type="Pending" /></td>
                 </tr>
             </table>
         </div>

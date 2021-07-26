d274f839f7faa2889bb777cfc3b1e7f2c2ed0255
ahmed-bs <medamed777@gmail.com>
2021-07-26 08:36:41 +0000

    chart

---
+++ reactproject/src/CData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'Janv',
+      "Active User": 4000,
+    },
+    {
+      name: 'Fevr',
+      "Active User": 8000,
+    },
+    {
+      name: 'Mars',
+      "Active User": 3000,
+    },
+    {
+      name: 'Avr',
+      "Active User": 9000,
+    },
+    {
+      name: 'Mais',
+      "Active User": 5000,
+    },
+    {
+      name: 'Juin',
+      "Active User": 4000,
+    },
+    {
+      name: 'Juil',
+      "Active User": 8000,
+    },
+    {
+      name: 'Aout',
+      "Active User": 4000,
+    },
+    {
+      name: 'Sept',
+      "Active User": 7000,
+    },
+    {
+      name: 'Oct',
+      "Active User": 6000,
+    },
+    {
+      name: 'Nov',
+      "Active User": 2000,
+    },
+    {
+      name: 'Dec',
+      "Active User": 4000,
+    },
+ 
+  ];
--- reactproject/src/pages/home/Home.jsx
+++ reactproject/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
+import Chart from "../../components/chart/Chart"
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
 import "./home.css"
-
+import { userData } from "../../CData"
 export default function Home() {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
         </div>
     )
 }

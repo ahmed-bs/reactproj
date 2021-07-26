f1869706ccfaa0154bed74db4aa725272172611f
ahmed-bs <medamed777@gmail.com>
2021-07-26 13:51:37 +0000

    users starting

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -2,19 +2,25 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css"
 import Home from "./pages/home/Home";
-function App(){
-  return(
-    <div  >
-<Topbar/>
-  <div className="container">
-    <Sidebar/>
-   <Home/>
-  </div>
+import { BrowserRouter as Router, Switch , Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+function App() {
+  return (
+    <Router  >
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList/>
+          </Route>
+        </Switch>
 
-
-
-   </div>
-
+      </div>
+    </Router>
   );
 }
 export default App;

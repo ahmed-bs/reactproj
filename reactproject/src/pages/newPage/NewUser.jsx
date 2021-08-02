4044521cf3d3c2ce76ea6180d8e861fab8f2cacc
ahmed-bs <medamed777@gmail.com>
2021-08-02 21:57:45 +0000

    next time

--- reactproject/src/pages/newPage/NewUser.jsx
+++ reactproject/src/pages/newPage/NewUser.jsx
@@ -3,7 +3,39 @@ import "./newUser.css"
 export default function NewUser() {
     return (
         <div className="newUser">
-           New User Page
+           <h1 className="newUserTitle">
+               New User
+           </h1>
+           <form  className="newUserForm">
+               <div className="newUserItem">
+                   <label >UserName</label>
+                   <input type="text" placeholder="someone"/>
+               </div>
+               <div className="newUserItem">
+                   <label >FullName</label>
+                   <input type="text" placeholder="someoneNm"/>
+               </div>
+               <div className="newUserItem">
+                   <label >Email</label>
+                   <input type="text" placeholder="someone@gmail.com"/>
+               </div>
+               <div className="newUserItem">
+                   <label >Password</label>
+                   <input type="text" placeholder="password"/>
+               </div>
+               <div className="newUserItem">
+                   <label >Phone</label>
+                   <input type="text" placeholder="+26 154 598 22"/>
+               </div>
+               <div className="newUserItem">
+                   <label >Adresse</label>
+                   <input type="text" placeholder="somewhere"/>
+               </div>
+               <div className="newUserItem">
+                   <label >Gender</label>
+                   <input type="radio" name="male" id="male" value="male"/>
+               </div>
+           </form>
         </div>
     )
 }
